a5f861ae8d27e3748d68aafe517f20d334940262
Rustam-xx7 <chakrabortyrustam@gmail.com>
2025-09-16 16:34:23 +0530

    title fix

--- app/layout.js
+++ app/layout.js
@@ -24,12 +24,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <Head>
-        <Script
-            src="https://cdn.lordicon.com/lordicon.js"
-            strategy="afterInteractive"
-          />
-      </Head> */}
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
