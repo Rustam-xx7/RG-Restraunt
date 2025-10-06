f952bc0d82c8c6d58c853c78f276d26db6418a57
Rustam-xx7 <chakrabortyrustam@gmail.com>
2025-10-06 10:18:59 +0530

    navbar update,06-10-25,01

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -26,17 +26,24 @@ const Navbar = () => {
           />
         </Link>
       </div>
-      <div className=" bg-ambe-300 w-full md:w-120 h-fit py-2 px-2 ">
-        <ul className="flex  h-full items-center w-full justify-around font-bold text-sm ">
+      <div className="w-full h-fit py-2 px-2 ">
+        <ul className="flex  h-full items-center w-full justify-start gap-10 px-2 md:px-6 font-bold text-sm ">
           <Link href="/">
-            <li>Home</li>
+            <li>
+              <lord-icon
+                src="https://cdn.lordicon.com/jeuxydnh.json"
+                trigger="click"
+                stroke="bold"
+                colors="primary:#ffffff,secondary:#eeca66"
+                style={{ width: "30px", height: "30px" }}
+              ></lord-icon>
+            </li>
           </Link>
           <Link href="/menu">
-            <li>Menu</li>
+            <li><button className=" w-fit px-8 py-2  bg-black/20 shadow-md shadow-black/30  font-serif font-extralight rounded-xl ">
+                Menu
+              </button></li>
           </Link>
-          {/* <Link href="/story">
-            <li>Story</li>
-          </Link> */}
         </ul>
       </div>
       <div className="flex items-center gap-2 md:gap-6 justify-center">
